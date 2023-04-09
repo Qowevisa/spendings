15ba155e53120c46e95053ed597a2e3682c61284
Qowevisa <qowevisa@gmail.com>
2023-04-09 17:49:06 +0300

    Add get_all_names/0 and validation for :name

--- lib/spendings/repo/type.ex
+++ lib/spendings/repo/type.ex
@@ -1,6 +1,8 @@
 defmodule Spendings.Repo.Type do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Spendings.Repo
+  import Ecto.Query, only: [from: 2]
 
   @timestamps_opts [type: :utc_datetime_usec]
 
@@ -13,5 +15,13 @@ defmodule Spendings.Repo.Type do
   def changeset(part, params \\ %{}) do
     part
     |> cast(params, [:name])
+    |> validate_required([:name])
+    |> unique_constraint(:name)
+  end
+
+  def get_all_names() do
+    Repo.all(
+      from t in "types", select: t.name
+    )
   end
 end
